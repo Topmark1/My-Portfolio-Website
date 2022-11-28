7d618f712d96bf0217b4a0298b1c78743b035928
Topmark1 <topsiology11@gmail.com>
2022-11-28 20:31:55 +0100

    my portfolio confirm

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+/* typing Animation */
+var typed = new Typed('.typing',{
+    strings:['','Frontend Developer','Web Developer','Graphic Designer','Digital Expert'],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
