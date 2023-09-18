0c2d73bc3211bccdd46c18afbc103f615f117a7c
Temitope Akande <topsiology11@gmail.com>
2023-09-18 08:32:06 +0100

    latest

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 /* typing Animation */
 var typed = new Typed('.typing',{
-    strings:['','Frontend Developer','Web Developer','Graphic Designer','Digital Expert','Web3 Dapp Builder'],
+    strings:['','Frontend Developer','Web3 Auditor','Web Developer','Graphic Designer','Digital Expert','Web3 Dapp Builder'],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
